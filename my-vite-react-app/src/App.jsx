181e043faf3a5648663275e9bb8c02494ab3a1a1
Axionev <axionevnick@gmail.com>
2025-10-28 10:36:14 +0100

    Initial commit

---
+++ my-vite-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Name from './components/React-homeWork01/Name'
+import Cars from './components/React-homeWork01/Cars'
+import Movies from './components/React-homeWork01/Movies'
+function App() {
+
+  return (
+    <>
+      <Name />
+      <Cars />
+      <Movies />
+    </>
+  )
+}
+export default App
---
+++ my-vite-react-app/src/components/React-homeWork01/Name.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Name = () => {
+    return (
+        <div>
+            <h1>Nikola Nevchev</h1>
+        </div>
+    )
+}
